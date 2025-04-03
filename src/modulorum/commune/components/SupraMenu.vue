d8131381772e7b8b43eca544c5514bd1d479a447
Kaue-oliveira-hub <kaueoliveiramcz@gmail.com>
2025-04-03 17:39:29 +0200

    creando menu interface importado de daiysUI

---
+++ src/modulorum/commune/components/SupraMenu.vue
@@ -0,0 +1,5 @@
+<template>
+
+<div class="flex items-center justify-between p-4 bg-base-200"></div>
+<h2 class="text-lg font-bold m-4 text-blue-600">Notion App</h2>
+</template>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,3 +1,4 @@
+import ProjectsLayout from '@/modulorum/projects/layouts/ProjectsLayout.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 
 
@@ -5,6 +6,12 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
    
+    {
+      path: '/',
+      name: 'projects',
+      component: ProjectsLayout,
+   }
+  
   ],
 })
 
