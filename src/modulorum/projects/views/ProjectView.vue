cae61868d20923aeea5a540447b489836d5d12f1
Kaue-oliveira-hub <kaueoliveiramcz@gmail.com>
2025-04-14 19:59:21 +0200

    BreadCrumbs

---
+++ src/modulorum/projects/views/ProjectView.vue
@@ -0,0 +1,31 @@
+<template>
+  <BreadCrumbs :nomen="project?.nomen ??'No name'"/>
+</template>
+
+<script setup lang="ts">
+import BreadCrumbs from '@/modulorum/commune/components/BreadCrumbs.vue';
+import { useProjectsStore } from '../stores/projects.store';
+import { ref, watch } from 'vue';
+import { Project } from '
+
+interface Props{
+    id:string;
+}
+const props = defineProps<Props>();
+
+const projectStore = useProjectsStore();
+
+const project = ref<Project>();
+
+watch (() => props.id,()=>{
+   project = projectStore.projectList.find(project => project.id === props.id )
+},
+{
+
+    immediate: true,
+    
+})
+
+
+
+</script>
